6deeed2cdf14c16f2ea6e5bb7c39cbb6d9e192e9
rsimmons11 <ptgrd1980@gmail.com>
2023-10-28 14:54:09 -0700

    added user property to associate with  comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,10 +1,15 @@
+// COMMENTS CONTROLLER
+
 const Comment = require("../models/Comment");
 
 module.exports = {
 
   createComment: async (req, res) => {
     try {
+
+      // Create a new comment and associate it with the user
       await Comment.create({
+        user: req.user.id,
         comment: req.body.comment,
         likes: 0,
         post: req.params.id,
