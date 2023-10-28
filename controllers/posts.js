8afc19df40c5f875c8c563bf0622349bc03d730d
rsimmons11 <ptgrd1980@gmail.com>
2023-10-28 14:58:02 -0700

    added populate() to retrieve the ref'd user doc

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,5 @@
+//POST CONTROLLER
+
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment");
@@ -22,7 +24,7 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
+      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).populate('user').lean();
       res.render("post.ejs", { post: post, user: req.user, comments: comments });
     } catch (err) {
       console.log(err);
